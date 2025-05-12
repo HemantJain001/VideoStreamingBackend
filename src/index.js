7b9fb417c33c33d39f6fd9cfcac480332dba1faf
HemantJain001 <137427928+HemantJain001@users.noreply.github.com>
2025-05-13 00:36:49 +0530

    Connecting Database

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import dotenv from 'dotenv/config';
+import express from 'express';
+import mongoose from 'mongoose';
+import connectDB from './db/index.js';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+connectDB();
+
+
+
+
+
+
+
+// (async ()=>{
+//     try {
+//         // Connect to MongoDB
+//         const MONGO_URI = process.env.MONGO_URI;
+//         await mongoose.connect(`${MONGO_URI}/${DB_NAME}`); 
+//         console.log('Connected to MongoDB');
+
+//     } catch (error) {
+//         console.error('Error connecting to MongoDB:', error);
+//     }
+// })()
